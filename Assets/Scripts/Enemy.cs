343473ef0ce9d43124bccfb3b49fb50bb024dfb4
BA-CONFERENCE-10 <BA-CONFERENCE-10@student.georgianc.on.ca>
2023-02-25 10:09:05 -0500

    Added gunfire trigger based on Field of View vs User.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,22 +8,34 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
 
     [SerializeField] private Animator animator;
-    private float currentTime;
+    private float currentTime, walkTime, runTime, deathTime;
     private int check = 0;
+    public int hp = 100;
 
     public GameObject user;
     public float walkSpeed = 0.6f;
     public float runSpeed = 1.2f;
+    public float viewRadius = 50f;
+    public LayerMask userMask;
+
+    private float viewAngle = 20f;
+    public float meshResolution = 0.5f;
+
+    public List<AudioClip> walkClips;
+    public List<AudioClip> runClips;
+    SpawnEnemy spawnEnemyInstance;
+
     void Start()
     {
         animator = transform.GetComponent<Animator>();
-        currentTime = 0;
+        currentTime = runTime = walkTime = 0;
+        spawnEnemyInstance = SpawnEnemy.instance;
     }
 
-    // Update is called once per frame
     void Update()
     {
         this.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        transform.Rotate(Vector3.zero);
 
         //Testing walk/run/idle animation
         if (currentTime > 3)
@@ -33,11 +45,16 @@ public class Enemy : MonoBehaviour
         }
         currentTime += Time.deltaTime;
 
+        if (hp <= 0)
+            check = 3;
 
-        if (Vector3.Distance(user.transform.position, transform.position) < 1)
+        if(DrawFieldOfView())
             return;
 
-        switch(check)
+        if (Vector3.Distance(user.transform.position, transform.position) < 1)
+            GetComponent<NavMeshAgent>().speed = 0;
+
+        switch (check)
         {
             case 0:
                 Idle();
@@ -47,6 +64,9 @@ public class Enemy : MonoBehaviour
                 break;
             case 2:
                 Run();
+                break;
+            case 3:
+                Death();
                 break;
             default:
                 Idle();
@@ -60,18 +80,100 @@ public class Enemy : MonoBehaviour
     {
         transform.GetComponent<NavMeshAgent>().speed = 0;
         animator.SetFloat("Speed", 0f, 0.1f, Time.deltaTime);
-        
+
     }
 
     void Walk()
     {
         transform.GetComponent<NavMeshAgent>().speed = walkSpeed;
-        animator.SetFloat("Speed", 0.5f, 0.1f, Time.deltaTime);
+        animator.SetFloat("Speed", 0.25f, 0.1f, Time.deltaTime);
+
+        if (walkTime > 0.6f)
+        {
+            GetComponent<AudioSource>().clip = walkClips[Random.Range(0, walkClips.Count)];
+            GetComponent<AudioSource>().Play();
+            walkTime = 0;
+        }
+        walkTime += Time.deltaTime;
     }
 
     void Run()
     {
         transform.GetComponent<NavMeshAgent>().speed = runSpeed;
-        animator.SetFloat("Speed", 1f, 0.1f, Time.deltaTime);
+        animator.SetFloat("Speed", 0.50f, 0.1f, Time.deltaTime);
+
+        if (runTime > 0.3f)
+        {
+            GetComponent<AudioSource>().clip = runClips[Random.Range(0, runClips.Count)];
+            GetComponent<AudioSource>().Play();
+            runTime = 0;
+        }
+        runTime += Time.deltaTime;
+    }
+
+    void Gunfire()
+    {
+        transform.GetComponent<NavMeshAgent>().speed = 0;
+        animator.SetFloat("Speed", 0.75f, 0.1f, Time.deltaTime);
+    }
+
+    public void Death()
+    {
+        transform.GetComponent<NavMeshAgent>().speed = 0;
+        animator.SetFloat("Speed", 1, 0.1f, Time.deltaTime);
+        deathTime += Time.deltaTime;
+        if (deathTime > 1.5f)
+        {
+            spawnEnemyInstance.list.Remove(gameObject);
+            Destroy(gameObject);
+        }
+
+    }
+
+    public void SetHealth(int hp)
+    {
+        this.hp = hp;
+    }
+
+    public int GetHealth() { return hp; }
+
+    bool DrawFieldOfView()
+    {
+        int stepCount = Mathf.RoundToInt(viewAngle * meshResolution);
+        float stepAngleSize = viewAngle / stepCount;
+        List<Vector3> viewPoints = new List<Vector3>();
+        for (int i = 0; i < stepCount; i++)
+        {
+            float angle = transform.eulerAngles.y - viewAngle / 2 + stepAngleSize * i;
+            //ViewCastInfo newViewCast = ViewCast(angle);
+            //viewPoints.Add(newViewCast.point);
+            if(DetectPlayer(angle))
+                return true;
+
+        }
+        return false;
+    }
+
+    Vector3 DirFromAngle(float angle, bool value)
+    {
+        if (!value)
+            angle += transform.eulerAngles.y;
+
+        return new Vector3(Mathf.Sin(angle * Mathf.Deg2Rad), 0, Mathf.Cos(angle * Mathf.Deg2Rad));
+    }
+
+
+    bool DetectPlayer(float angle)
+    {
+        Vector3 dir = DirFromAngle(angle, true);
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, dir, out hit, viewRadius, userMask)) { 
+            Gunfire();
+            return true;
+        }
+
+        return false;
+
     }
 }
--- Assets/Scripts/SpawnEnemy.cs
+++ Assets/Scripts/SpawnEnemy.cs
@@ -8,36 +8,154 @@ public class SpawnEnemy : MonoBehaviour
     public GameObject enemy;
     public GameObject user;
     private float currentTime = 0f;
-    
+    public static SpawnEnemy instance;
+    public List<GameObject> list;
 
-    private Stack<GameObject> stack;
+    public float meshResolution;
+    private float viewAngle = 365f;
+    private float viewRadius = 50f;
+    public LayerMask obstacleMask;
+
+    public MeshFilter meshFilter;
+    public Mesh mesh;
+    private bool readyToSpawn;
+    public float spawnTime;
+
+    public struct ViewCastInfo
+    {
+        public bool hit;
+        public Vector3 point;
+        public float dist;
+        public float angle;
+
+        public ViewCastInfo(bool _hit, Vector3 _point, float _dist, float _angle)
+        {
+            hit = _hit;
+            point = _point;
+            dist = _dist;
+            angle = _angle; 
+        }
+    }
+
+    void Awake()
+    {
+        if (instance != null)
+            Destroy(gameObject);
+        else 
+            instance = this;
+    }
+
     void Start()
     {
-        stack = new Stack<GameObject>();
+        //To view FOV
+        /*mesh = new Mesh();
+        mesh.name = "View mesh";
+        meshFilter.mesh = mesh;*/
+
+        list = new List<GameObject>();
+        
         SpawnNewEnemy(new Vector3(Random.Range(0, 10), Random.Range(0, 10), Random.Range(0, 10)));
+        
+       
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        if (currentTime > 10f)
+        DrawFieldOfView();
+        if (currentTime > spawnTime)
         {
+            readyToSpawn = true;
             currentTime = 0f;
-            if (stack.Count > 4)
-                Destroy(stack.Pop());
-
-            SpawnNewEnemy(new Vector3(Random.Range(0, 10), Random.Range(0, 10), Random.Range(0, 10)));
-
-           
         }
         currentTime += Time.deltaTime;
-
     }
 
     void SpawnNewEnemy(Vector3 coordinates)
     {
         GameObject newEnemy = Instantiate(enemy, coordinates, Quaternion.identity);
         newEnemy.GetComponent<Enemy>().user = user;
-        stack.Push(newEnemy);
+        list.Add(newEnemy);
+        Debug.Log(list.Count);
+        readyToSpawn = false;
+    }
+
+    Vector3 GetVectorFromAngle(float angle)
+    {
+        float angleRad = angle * Mathf.PI / 180f;
+        return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
+    }
+
+    void DrawFieldOfView()
+    {
+        int stepCount = Mathf.RoundToInt(viewAngle * meshResolution);
+        float stepAngleSize = viewAngle / stepCount;
+        List<Vector3> viewPoints = new List<Vector3>();
+        for(int i = 0; i < stepCount; i++)
+        {
+            float angle = transform.eulerAngles.y - viewAngle / 2 + stepAngleSize * i;
+            //ViewCastInfo newViewCast = ViewCast(angle);
+            //viewPoints.Add(newViewCast.point);
+            if (readyToSpawn)
+                GetCoordinatesForSpawn(angle);
+            
+        }
+
+
+        //Used to view FOV
+        /*int vertexCount = viewPoints.Count + 1;
+        Vector3[] vertices = new Vector3[vertexCount];
+        int[] triangles = new int[(vertexCount-2)*3];
+
+        vertices[0] = Vector3.zero;
+
+        for(int i = 0; i < vertexCount-1; i++)
+        {
+            vertices[i + 1] = transform.InverseTransformPoint(viewPoints[i]);
+            if (i < vertexCount - 2)
+            {
+                triangles[i * 3] = 0;
+                triangles[i * 3 + 1] = i + 1;
+                triangles[i * 3 + 2] = i + 2;
+            }
+        }
+
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();*/
+    }
+
+    Vector3 DirFromAngle(float angle, bool value)
+    {
+        if (!value)
+            angle += transform.eulerAngles.y;
+
+        return new Vector3(Mathf.Sin(angle * Mathf.Deg2Rad), 0, Mathf.Cos(angle * Mathf.Deg2Rad));
+    }
+
+    //For Viewing FOV
+    /*ViewCastInfo ViewCast(float globalAngle)
+    {
+        Vector3 dir = DirFromAngle(globalAngle, true);
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position, dir, out hit, viewRadius, obstacleMask))
+            return new ViewCastInfo(true, hit.point, hit.distance, globalAngle);
+        else
+            return new ViewCastInfo(false, transform.position + dir * viewRadius, viewRadius, globalAngle);
+
+    }*/
+
+    void GetCoordinatesForSpawn(float angle)
+    {
+        Vector3 dir = DirFromAngle(angle, true);
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, dir, out hit, viewRadius, obstacleMask))
+        {
+            Vector3 position = hit.collider.gameObject.transform.position + dir * 3f;
+            SpawnNewEnemy(position);
+        }
     }
 }
