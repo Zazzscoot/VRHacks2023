7f7aabd0f3002317fe204bb503bba386e6537d04
BA-CONFERENCE-07 <BA-CONFERENCE-07@student.georgianc.on.ca>
2023-02-25 07:32:08 -0500

    Added difficulty menu and gamecontroller script

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -4,13 +4,40 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MenuController : MonoBehaviour
 {
+    public GameObject MainMenu;
+    public GameObject DifficultyMenu;
     public void StartBtn()
     {
-        SceneManager.LoadScene("SampleScene");
+        MainMenu.SetActive(false);
+        DifficultyMenu.SetActive(true);
     }
 
     public void EndBtn()
     {
         Application.Quit();
+    }
+
+    public void Easy()
+    {
+        GameManager.Instance.difficulty = 1;
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Medium()
+    {
+        GameManager.Instance.difficulty = 2;
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Hard()
+    {
+        GameManager.Instance.difficulty = 3;
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void INSANE()
+    {
+        GameManager.Instance.difficulty = 4;
+        SceneManager.LoadScene("SampleScene");
     }
 }
